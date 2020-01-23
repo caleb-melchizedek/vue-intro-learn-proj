0dbdeb501f44baceb9b751c2dfe1220623657344
caleb-melchizedek <caleb.melchizedek@acity.edu.gh>
2020-01-23 17:54:25 +0000

    working on new vue powred quiz app. learn vue

---
+++ quiz/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    <b-nav tabs>
+        <b-nav-item disabled><b> Quiz App</b></b-nav-item>
+        <b-nav-item disabled><b>Counter: 4/10</b></b-nav-item>
+    </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ quiz/src/components/QuestionBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="Question-Box-Container"> 
+    <b-jumbotron>
+        
+    <template v-slot:lead>
+      {{currentQuestion.question}}
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      List of answers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#" @click="next">
+        Next
+    </b-button>
+
+    </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        currentQuestion:Object,
+        next:Function
+    },
+}
+</script>
